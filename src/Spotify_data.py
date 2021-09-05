e1933e553dbe4e1367c12bf4192e58124d87af13
Dr4kn <adrian@sprengels.de>
2021-09-05 23:58:21 +0200

    spotipy version 2.19.0 work now and the code is cleaned up

--- src/Spotify_data.py
+++ src/Spotify_data.py
@@ -1,17 +1,10 @@
 # only used for the gDrive upload
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import sqlite3
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-
-import os
-
-# necessary for the authentication and getting the json from spotify
-# pip install spotipy
-import spotipy
-import spotipy.util as util
-import spotipy.oauth2 as oauth2
-import sqlite3
-import sys
-import time
 
 
 # creates an sql table if one isn't their yet
@@ -19,35 +12,24 @@ def create_table():
     try:
         sql_cursor.execute("CREATE TABLE spotify_data (played_at TEXT, artists TEXT, album TEXT, "
                            "track TEXT, artists_id TEXT, album_id TEXT, track_id TEXT, duration_ms INTEGER)")
+        print("created spotify_data.db")
 
     except sqlite3.OperationalError:
         print("Sqlite Error")
 
 
-def environment_variables():
-    spotipy_client_id = os.environ.get('SPOTIPY_CLIENT_ID')
-    spotipy_client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
-    spotipy_redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')  # https://google.com/ can be used
-    spotipy_username = os.environ.get('SPOTIPY_USERNAME')
-    spotify_parser(spotipy_client_id, spotipy_client_secret, spotipy_redirect_uri, spotipy_username)
-
-
-def spotify_parser(client_id, client_secret, redirect_uri, username):
+# gets your last 50 songs you played
+def spotify_parser():
+    # scopes can be looked up here:
+    # https://developer.spotify.com/documentation/general/guides/scopes/
     scope = "user-read-currently-playing user-read-playback-state user-modify-playback-state user-read-recently-played"
-    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-    spotify_object = spotipy.Spotify(auth=token)
-    spotify_object.trace = False
-    save_multiple_songs(spotify_object.current_user_recently_played(limit=50))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    save_multiple_songs(sp.current_user_recently_played(limit=50))
 
 
 # goes into sql database and adds at max the last 50 songs at the bottom of the list chronologically
 # duplicates are ignored
 def save_multiple_songs(current_user_recently_played):
-    current_user_recently_played = spotipy()
-
-    # no idea how efficient it is but it doesn't matter much
-    # You run the script at at maximum every 15 minutes 
-    # If it would take one minute (which it doesn't) it really doesn't matter
     sql_cursor.execute("SELECT * FROM spotify_data ORDER BY played_at DESC LIMIT 1")
     last_played = sql_cursor.fetchone()
     i = len(current_user_recently_played["items"]) - 1
@@ -57,25 +39,24 @@ def save_multiple_songs(current_user_recently_played):
     # this ensures that only the new plays are saved without looping two times through the list
     while i >= 0:
         track = current_user_recently_played["items"][i]
-        time = track["played_at"]
+        played_at = track["played_at"]
         artist = track["track"]["album"]["artists"][0]["name"]
         album = track["track"]["album"]["name"]
-        song = track["track"]["name"]
+        track = track["track"]["name"]
         artist_id = track["track"]["album"]["artists"][0]["id"]
         album_id = track["track"]["album"]["id"]
-        song_id = track["track"]["id"]
+        track_id = track["track"]["id"]
         duration_ms = track["track"]["duration_ms"]
-        spotipy_data = (time, artist, album, song, artist_id, album_id, song_id, duration_ms)
+        spotipy_data = (played_at, artist, album, track, artist_id, album_id, track_id, duration_ms)
         sql_cursor.execute("INSERT INTO spotify_data VALUES (?, ?, ?, ?, ?, ?, ?, ?)", spotipy_data)
         i -= 1
 
+        # either you go through the list twice and check if the duplicate exists
+        # or you through it once and rollback if you find it
         if spotipy_data == last_played:
             sql.rollback()
 
     sql.commit()
-
-    # comment out if you don't want to upload it
-    # google_drive_upload()
 
 
 # creates gDrive credentials if they don't exist and uploads it automatically to it after that
@@ -105,13 +86,16 @@ def google_drive_upload():
 
 
 if __name__ == "__main__":
-    # gets directory of this python file
+
+    # gets directory of this exact python file
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     sql = sqlite3.connect(dir_path + "/spotify_data.db")
     sql_cursor = sql.cursor()
-    create_table()
 
-    environment_variables()
+    create_table()
+    spotify_parser()
+    # comment out if you don't want to upload it
+    # google_drive_upload()
 
     print("Done")
